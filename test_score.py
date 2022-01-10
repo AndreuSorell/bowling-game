4e6a0a9e35e34a2c76d2d4a5c6dfea3c94ea7ae8
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-10 01:54:04 +0100

    test(test): Crea casos test para la funcion de una jugada simple

---
+++ test_score.py
@@ -0,0 +1,23 @@
+from bowling_score import Bolos
+
+simple_score = Bolos('9-9-9-9-9-9-9-9-9-9-')
+simple_score2 = Bolos('9-9-9-9-9-9-9-9-9-8-')
+simple_score3 = Bolos('72729-9-9-9-9-9-9-72')
+simple_score4 = Bolos('9-3561368153258-7181')
+simple_score5 = Bolos('12345123451234512345')
+no_simple_score = Bolos('9X9-9-9-9-9-9-9-9-9-')
+no_simple_score2 = Bolos('9/9-9-9-9-9-9-9-9-9-')
+
+
+""" simple_score.simple_pins()
+assert simple_score.simple_pins() == 90 """
+
+def test_simple_pins():
+    assert 90 == simple_score.simple_pins()
+    assert 89 != simple_score.simple_pins()
+    assert 89 == simple_score2.simple_pins()
+    assert 90 == simple_score3.simple_pins()
+    assert 82 == simple_score4.simple_pins()
+    assert 60 == simple_score5.simple_pins()
+    assert 'No es un calculo simple' == no_simple_score.simple_pins()
+    assert 'No es un calculo simple' == no_simple_score2.simple_pins()
