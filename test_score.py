62da18b9a5cf850777a249ddc028277dc907dea3
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-10 03:35:34 +0100

    test(test): Añade un caso test para comprobar la juagada con spares

--- test_score.py
+++ test_score.py
@@ -8,10 +8,6 @@ simple_score5 = Bolos('12345123451234512345')
 no_simple_score = Bolos('9X9-9-9-9-9-9-9-9-9-')
 no_simple_score2 = Bolos('9/9-9-9-9-9-9-9-9-9-')
 
-
-""" simple_score.simple_pins()
-assert simple_score.simple_pins() == 90 """
-
 def test_simple_pins():
     assert 90 == simple_score.simple_pins()
     assert 89 != simple_score.simple_pins()
@@ -21,3 +17,9 @@ def test_simple_pins():
     assert 60 == simple_score5.simple_pins()
     assert 'No es un calculo simple' == no_simple_score.simple_pins()
     assert 'No es un calculo simple' == no_simple_score2.simple_pins()
+
+score_spares1 = Bolos('5/5/5/5/5/5/5/5/5/5/5')
+
+def test_pins_with_spares():
+    assert 150 == score_spares1.pins_with_spares()
+
