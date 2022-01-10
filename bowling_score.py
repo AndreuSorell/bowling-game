8c836051dd84e4cf90bc46ec9295707144fadc53
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-10 03:33:34 +0100

    feat(src): Crea una funcion para calcular la jugada cuando hay spares

--- bowling_score.py
+++ bowling_score.py
@@ -3,27 +3,51 @@ class Bolos:
         self.pins = list(pins)
         self.score = 0
 
-    def check_no_strike(self):
+    def check_there_strike(self):
         if self.pins.count('X') == 0:
             return False
         return True
     
-    def check_no_split(self):
+    def check_there_spare(self):
         if self.pins.count('/') == 0:
             return False
         return True
+    
+    def nulo(self):
+        i = 0
+        for char in self.pins:
+            if char == '-':
+                self.pins[i] = '0'
+                i += 1
+            else:
+                i += 1
 
     def simple_pins(self):
-        if Bolos.check_no_strike(self) or Bolos.check_no_split(self):
+        Bolos.nulo(self)
+        if Bolos.check_there_strike(self) or Bolos.check_there_spare(self):
             return 'No es un calculo simple'
         for char in self.pins:
-                if char == '-':
+            self.score += int(char)
+        return self.score
+
+    def pins_with_spares(self):
+        Bolos.nulo(self)
+        if Bolos.check_there_strike(self):
+            return 'Hay uno o mas strikes en esta jugada'
+        i = 0
+        for char in self.pins:
+            if char == '/':
+                self.score = (self.score + 10 + int(self.pins[i + 1]))
+                i += 1
+            else:
+                """ if self.pins[i + 1] == '/':
+                    i += 1
                     continue
-                else:
-                    self.score += int(char)
+                self.score += int(char) """
+                i += 1
         return self.score
 
 
 if __name__ == '__main__':
     tiradas1 = Bolos('5/5/5/5/5/5/5/5/5/5/5')
-    print(tiradas1.simple_pins())
+    print(tiradas1.pins_with_spares())
