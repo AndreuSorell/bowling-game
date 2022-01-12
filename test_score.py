cdfe3fff1a11057cbb452b5ff85bcb5271d8c8ed
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-13 00:55:48 +0100

    test(test): Añade para jugadas con strikes sin spares

--- test_score.py
+++ test_score.py
@@ -24,3 +24,15 @@ score_spares2 = Bolos('9-3/613/815/-/8-7/8/8')
 def test_pins_with_spares():
     assert 150 == score_spares1.score_bolos()
     assert 131 == score_spares2.score_bolos()
+
+def test_with_strikes():
+    assert 100 == Bolos('X9-9-9-9-9-9-9-9-9-').score_bolos()
+    assert 100 == Bolos('9-9-9-9-9-9-9-9-9-X9-').score_bolos()
+    assert 110 == Bolos('X9-X9-9-9-9-9-9-9-').score_bolos()
+    assert 120 == Bolos('XX9-9-9-9-9-9-9-9-').score_bolos()
+    assert 141 == Bolos('XXX9-9-9-9-9-9-9-').score_bolos()
+    assert 111 == Bolos('9-9-9-9-9-9-9-9-9-XXX').score_bolos()
+    assert 300 == Bolos('XXXXXXXXXXXX').score_bolos()
+
+def test_spares_and_strikes():
+    assert 149 == Bolos('8/549-XX5/53639/9/X').score_bolos()
