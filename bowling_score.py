cf9571bf618ed6b16b4fde143046a81ae2a977a9
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-13 00:12:16 +0100

    feat(src): Modifica una funcion para poder sacar el resultado con spares en las rows

--- bowling_score.py
+++ bowling_score.py
@@ -2,6 +2,9 @@ class Bolos:
     def __init__(self, pins = []):
         self.pins = list(pins)
         self.score = 0
+        self.previous_pin = 0
+        self.next_pin = 0
+        self.row = 20
 
     def check_there_strike(self):
         if self.pins.count('X') == 0:
@@ -12,6 +15,11 @@ class Bolos:
         if self.pins.count('/') == 0:
             return False
         return True
+
+    def spare(self): ##
+        self.score += ((10 - int(self.pins[self.previous_pin])) + int(self.pins[self.next_pin]))
+
+    # def strikes(self):
     
     def nulo(self):
         i = 0
@@ -47,7 +55,26 @@ class Bolos:
                 i += 1
         return self.score
 
+    def score_bolos(self):
+        Bolos.nulo(self)
+        if Bolos.check_there_strike(self):
+            return 'Hay uno o mas strikes en esta jugada'
+        i = 0
+        while i < self.row:
+            if self.pins[i] == '/':
+                Bolos.spare(self)
+                i += 1
+                self.previous_pin = i-1
+                self.next_pin = i+1
+            else:
+                self.score += int(self.pins[i])
+                i += 1
+                self.previous_pin = i-1
+                self.next_pin = i+1
+
+        return self.score
+
 
 if __name__ == '__main__':
     tiradas1 = Bolos('5/5/5/5/5/5/5/5/5/5/5')
-    print(tiradas1.pins_with_spares())
+    print(tiradas1.score_bolos())
